ff31b72d4630a8eef91931722a77f1a519593611
sebasttianvelez <sebasttianvelez@gmail.com>
2019-04-24 11:32:52 -0500

    Crear servidor con NODEJS

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/*
+cargar modulos de expres y bodyParser para recibir peticiones http
+y todo lo que recibamos en el api convertirlo a json (usable con javascript)
+*/
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//llamar a express
+
+//express es nuestro controlador http
+var app = express();
+
+
+
+//bodyparser
+/* Un middleware es una instruccion que se ejecuta antes de cualquier
+petición http, es decir cuando hacemos una petición al API primero que todo
+se ejecuta el middleware y despues el resto de cosas */
+
+//definir un middleware de boddyparser
+
+/* Lo que hace esta funcion es convertir los datos que nos lleguen por post o put en json */
+app.use(bodyParser.urlencoded({extended:false}));
+
+//convertir a json
+app.use(bodyParser.json());
+
+//Configurar CORS O cabeceras
+
+//rutas
+
+/* La ruta pruebas-api tiene una funcion de callback
+recibe req-> request (lo que llega) y response (lo que devuelve)*/
+
+app.get('/pruebas-api', (req, res)=>{
+    //el codigo 200 indica que esta bien
+    //El send envia un mensaje en json
+    res.status(200).send(
+        /*
+        {
+            menssage: 'Esta ruta es de prueba en mi API Restful con mongo y node'
+        }
+        */
+        "<h1>pagina de inicio</h1>"
+    );
+})
+
+//hacer exportable el modulo app para ser cargado en el index.js
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@
 
 //cargar mongoose
 var mongoose = require('mongoose');
+//crear una variable app para cargar modulos app.js
+var app = require('./app');
+
+//indicar el puerto que va a tener el API
+var port = 3700;
+
+
 
 //indicar que mongoose es una promesa
 
@@ -10,5 +17,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/portafolio') //conectar a la base de datos
         .then(() => { //si logra conectarse
             console.log("Conexión a la base de datos establecida con éxito");
+            //crear servidor:
+            app.listen(port, () => {
+                console.log("El servidor esta corriendo en localhost:3700");
+            });
         })
         .catch(err => console.log(err));
